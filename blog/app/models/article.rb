6e9dc77a91b60d7656c8a841c20887f31ca57626
EC2 Default User <ec2-user@ip-172-31-44-169.ec2.internal>
2021-05-17 22:01:09 +0000

    First commit

---
+++ blog/app/models/article.rb
@@ -0,0 +1,11 @@
+class Article < ApplicationRecord
+    
+    has_many :  has_many :comments, dependent: :destroy
+
+
+    
+    
+    validates :title, presence: true,
+                    length: { minimum: 5 }
+    
+end
